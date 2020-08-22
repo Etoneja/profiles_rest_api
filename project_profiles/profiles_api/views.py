162adeba4bf9361ed6735351778f6f735cbd601f
Etoneja <mikhail.zanozin@gmail.com>
2020-08-23 01:36:48 +0300

    added apiview

--- project_profiles/profiles_api/views.py
+++ project_profiles/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            "feature 1",
+            "feature 2",
+            "feature 3",
+        ]
+
+        return Response({
+            "message": "Hello!",
+            "an_apiview": an_apiview
+        })
