186aa6d1e487a713ac1f121d0ad283c3da16c829
Etoneja <mikhail.zanozin@gmail.com>
2020-08-23 04:33:37 +0300

    methods to apiview

--- project_profiles/profiles_api/views.py
+++ project_profiles/profiles_api/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+from .models import UserProfile
 
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
 
@@ -17,4 +23,41 @@ class HelloApiView(APIView):
         return Response({
             "message": "Hello!",
             "an_apiview": an_apiview
+        })
+
+    def post(self, request):
+
+        serializer = serializers.HelloSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            name = serializer.data.get("name")
+
+            message = f"Hello {name}"
+
+            return Response({
+                "message": message
+            })
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+
+        return Response({
+            "method": "put"
+        })
+
+    def patch(self, request, pk=None):
+
+        return Response({
+            "method": "patch"
+        })
+
+    def delete(self, request, pk=None):
+
+        return Response({
+            "method": "delete"
         })
