4b0df16add4546fa233e980085b3541732ed30f0
Etoneja <mikhail.zanozin@gmail.com>
2020-08-23 21:04:15 +0300

    added method to viewset

--- project_profiles/profiles_api/views.py
+++ project_profiles/profiles_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -57,6 +58,65 @@ class HelloApiView(APIView):
         })
 
     def delete(self, request, pk=None):
+
+        return Response({
+            "method": "delete"
+        })
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+
+        a_viewset = [
+            "bla 1",
+            "bla 2",
+            "bla 3"
+        ]
+
+        return Response({
+            "message": "Hello",
+            "a_viewset": a_viewset
+        })
+
+    def create(self, request):
+
+        serializer = serializers.HelloSerializer(
+            data=request.data
+        )
+        if serializer.is_valid():
+            name = serializer.data.get("name")
+            message = f"Hello {name}"
+            return Response({
+                "message": message
+            })
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+
+        return Response({
+            "method": "get"
+        })
+
+    def update(self, request, pk=None):
+
+        return Response({
+            "method": "put"
+        })
+
+    def partial_update(self, request, pk=None):
+
+        return Response({
+            "method": "patch"
+        })
+
+    def destroy(self, request, pk=None):
 
         return Response({
             "method": "delete"
